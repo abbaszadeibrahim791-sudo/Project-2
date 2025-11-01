5563085f02dd6ea5792fba696139675758439ffc
“Ibrahim” <abbaszadeibrahim791@gmail.com>
2025-11-01 21:08:27 +0400

    The day of cat

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import random
+class Cat:
+    def __init__(self,name):
+        self.name=name
+        self.hunger=50
+        self.energy=50
+        self.happiness=50
+        self.alive=True
+    def eat(self):
+        print("The cat is eating")
+        self.hunger+=10
+        self.energy+=5
+        self.happiness+=2
+    def sleep(self):
+        print("The cat is sleeping")
+        self.energy+=15
+        self.hunger-=5
+    def play(self):
+        print("The cat is playing")
+        self.happiness+=10
+        self.energy-=10
+        self.hunger-=5
+    def is_alive(self):
+        if self.hunger<=0:
+            print("The cat starved")
+            self.alive=False
+        elif self.energy<=0:
+            print("The cat became tired")
+            self.alive=False
+        elif self.happiness <=0:
+            print("The cat is sad")
+            self.alive=False
+    def end_of_day(self):
+        print(f"Hunger={self.hunger}")
+        print(f"Energy={self.energy}")
+        print(f"Happiness={self.happiness}")
+    def live(self,day):
+        print(f"Day{day} of {self.name}'s life")
+        action=random.randint(1,3)
+        if action==1:
+            self.eat()
+        elif action==2:
+            self.sleep()
+        elif action==3:
+            self.play()
+        self.end_of_day()
+        self.is_alive()
+my_cat=Cat("Mestan")
+for day in range(1,31):
+    if not my_cat.alive:
+        break
+    my_cat.live(day)
